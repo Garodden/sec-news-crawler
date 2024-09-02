6947263bd9afb92865158b03e7e028a77fced38d
seocd777 <seocd777@estsecurity.com>
2024-09-02 16:41:18 +0900

    🔧 fix: init.sql and added domain_path table

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -1,42 +1,38 @@
 
 CREATE TABLE users (
-  id integer NOT NULL,
-  email VARCHAR(127) NOT NULL, 
+  id SERIAL PRIMARY KEY,  
+  email VARCHAR(127) NOT NULL UNIQUE, 
   followed_date TIMESTAMP  
 );
 
 
 CREATE TABLE article_domain (
-  id integer NOT NULL,
-  domain_name VARCHAR(127) NOT NULL  
+  id SERIAL PRIMARY KEY,  
+  domain VARCHAR(127) NOT NULL UNIQUE
 );
 
 
 CREATE TABLE article_info (
-  id integer NOT NULL,  
+  id SERIAL PRIMARY KEY,   
   title VARCHAR(127) NOT NULL,  
   url VARCHAR(127) NOT NULL,  
   domain_id INTEGER,  
-  sim_hash_vale INTEGER,  
+  sim_hash_value INTEGER,  
   published_at TIMESTAMP,  
   CONSTRAINT fk_article_info_domain_id FOREIGN KEY (domain_id) REFERENCES article_domain(id)  
 );
 
+CREATE TABLE article_path (
+  id SERIAL PRIMARY KEY,  
+  domain_id INTEGER,
+  path VARCHAR(127),
+  CONSTRAINT fk_article_path_domain_id FOREIGN KEY (domain_id) REFERENCES article_domain(id)  
+);
 
 CREATE TABLE subscribe_info (
-  id integer NOT NULL,
-  user_id INTEGER,  
-  domain_id INTEGER,  -
+  id SERIAL PRIMARY KEY,  
+  user_id INTEGER,
+  domain_id INTEGER,
   CONSTRAINT fk_subscribe_info_user_id FOREIGN KEY (user_id) REFERENCES users(id),  
   CONSTRAINT fk_subscribe_info_domain_id FOREIGN KEY (domain_id) REFERENCES article_domain(id)  
 );
-
-
-ALTER TABLE article_info
-  ADD CONSTRAINT fk_article_info_domain_id FOREIGN KEY (domain_id) REFERENCES article_domain(id);
-
-ALTER TABLE subscribe_info
-  ADD CONSTRAINT fk_subscribe_info_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-
-ALTER TABLE subscribe_info
-  ADD CONSTRAINT fk_subscribe_info_domain_id FOREIGN KEY (domain_id) REFERENCES article_domain(id);
